ff08869278c83f1dcf1eab1d772faf2ac2a09b5c
enjoyneering <enjoyneering79@gmail.com>
2018-06-18 13:11:34 -0400

    Update MAX31855_Two_Sensors_hw_SPI_Demo.ino

--- examples/MAX31855_Two_Sensors_hw_SPI_Demo/MAX31855_Two_Sensors_hw_SPI_Demo.ino
+++ examples/MAX31855_Two_Sensors_hw_SPI_Demo/MAX31855_Two_Sensors_hw_SPI_Demo.ino
@@ -5,15 +5,7 @@
   written by : enjoyneering79
   sourse code: https://github.com/enjoyneering/MAX6675
 
-  This sensor uses SPI bus to communicate, specials pins are required to interface
-
-  Connect chip to pins:    SCLK        MISO        don't use SS for CS  don't use MOSI for CS
-  Uno, Mini, Pro:          13          12          10                   11
-  Mega2560, Due:           52          50          53                   51
-  Leonardo, ProMicro:      15          14          x                    16
-  NodeMCU 1.0:             GPIO14/D5   GPIO12/D6   GPIO15/D8            GPIO13/D7
-  WeMos D1 Mini:           GPIO14/D5   GPIO12/D6   GPIO15/D8            GPIO13/D7
-
+  - MAX31855 maximum power supply voltage is 3.6v
   - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
   - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
     with 0.25°C resolution/increment.
@@ -23,6 +15,25 @@
     near the converter because this may produce an errors.
   - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
     the T+ and T- pins, to filter noise on the thermocouple lines.
+
+  This sensor uses SPI bus to communicate, specials pins are required to interface
+  Board:                                    SCLK        MISO        SS don't use for CS   MOSI
+  Uno, Mini, Pro, ATmega168, ATmega328..... 13          12          10                    11
+  Mega2560, Due............................ 52          50          53                    51
+  Leonardo, ProMicro, ATmega32U4........... 15          14          x                     16
+  Blue Pill, STM32F103xxxx boards.......... PA5**       PA6**       PA4**                 PA7**
+  NodeMCU 1.0, WeMos D1 Mini............... GPIO14/D5   GPIO12/D6   GPIO15/D8*            GPIO13/D7
+
+                                           *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
+                                            pullup-down resistor
+                                          **STM32F103xxxx pins are NOT 5v tolerant, bi-directional
+                                            logic level converter is needed
+
+  Frameworks & Libraries:
+  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+  ESP8266 Core          - https://github.com/esp8266/Arduino
+  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
   GNU GPL license, all text above must be included in any redistribution, see link below for details:
   - https://www.gnu.org/licenses/licenses.html
@@ -53,10 +64,10 @@ void setup()
 
   while (myMAX31855_01.getChipID() != MAX31855_ID)
   {
-    Serial.println("MAX6675_01 error");
+    Serial.println(F("MAX6675_01 error")); //(F()) saves string to flash & keeps dynamic memory free
     delay(5000);
   }
-  Serial.println("MAX6675_01 OK");
+  Serial.println(F("MAX6675_01 OK"));
 }
 
 void loop()
@@ -66,16 +77,19 @@ void loop()
     switch (myMAX31855_01.detectThermocouple())
     {
       case MAX31855_THERMOCOUPLE_SHORT_TO_VCC:
-        Serial.println("Thermocouple_01 short to VCC");
+        Serial.println(F("Thermocouple_01 short to VCC"));
         break;
+
       case MAX31855_THERMOCOUPLE_SHORT_TO_GND:
-        Serial.println("Thermocouple_01 short to GND");
+        Serial.println(F("Thermocouple_01 short to GND"));
         break;
+
       case MAX31855_THERMOCOUPLE_NOT_CONNECTED:
-        Serial.println("Thermocouple_01 not connected");
+        Serial.println(F("Thermocouple_01 not connected"));
         break;
+
       case MAX31855_THERMOCOUPLE_UNKNOWN:
-        Serial.println("Thermocouple_01 unknown error, check spi cable");
+        Serial.println(F("Thermocouple_01 unknown error, check spi cable"));
         break;
     }
     delay(5000);
@@ -84,22 +98,22 @@ void loop()
   rawData_01 = myMAX31855_01.readRawData();
   rawData_02 = myMAX31855_02.readRawData();
 
-  Serial.print("Chip_ID_01: ");
+  Serial.print(F("Chip_ID_01: "));
   Serial.println(myMAX31855_01.getChipID(rawData_01));
 
-  Serial.print("Chip_ID_02: ");
+  Serial.print(F("Chip_ID_02: "));
   Serial.println(myMAX31855_02.getChipID(rawData_02));
 
-  Serial.print("Cold_Junction_01: ");
+  Serial.print(F("Cold_Junction_01: "));
   Serial.println(myMAX31855_01.getColdJunctionTemperature(rawData_01));
 
-  Serial.print("Cold_Junction_02: ");
+  Serial.print(F("Cold_Junction_02: "));
   Serial.println(myMAX31855_02.getColdJunctionTemperature(rawData_02));
 
-  Serial.print("Thermocouple_01: ");
+  Serial.print(F("Thermocouple_01: "));
   Serial.println(myMAX31855_01.getTemperature(rawData_01));
 
-  Serial.print("Thermocouple_02: ");
+  Serial.print(F("Thermocouple_02: "));
   Serial.println(myMAX31855_02.getTemperature(rawData_02));
 
   delay(5000);
