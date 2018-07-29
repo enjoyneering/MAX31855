030fea833106ea6fbf8b5e44bb33105a90b1f1ce
enjoyneering <enjoyneering79@gmail.com>
2018-07-29 11:38:05 -0400

    Update MAX31855_Two_Sensors_hw_SPI_Demo.ino

--- examples/MAX31855_Two_Sensors_hw_SPI_Demo/MAX31855_Two_Sensors_hw_SPI_Demo.ino
+++ examples/MAX31855_Two_Sensors_hw_SPI_Demo/MAX31855_Two_Sensors_hw_SPI_Demo.ino
@@ -1,42 +1,42 @@
 /***************************************************************************************************/
 /* 
-  Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
+   Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
 
-  written by : enjoyneering79
-  sourse code: https://github.com/enjoyneering/MAX6675
+   written by : enjoyneering79
+   sourse code: https://github.com/enjoyneering/MAX6675
 
-  - MAX31855 maximum power supply voltage is 3.6v
-  - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
-  - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
-    with 0.25°C resolution/increment.
-  - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
-    Optimal performance of cold junction compensation happends when the thermocouple cold junction
-    & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
-    near the converter because this may produce an errors.
-  - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
-    the T+ and T- pins, to filter noise on the thermocouple lines.
+   - MAX31855 maximum power supply voltage is 3.6v
+   - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
+   - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
+     with 0.25°C resolution/increment.
+   - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
+     Optimal performance of cold junction compensation happends when the thermocouple cold junction
+     & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
+     near the converter because this may produce an errors.
+   - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
+     the T+ and T- pins, to filter noise on the thermocouple lines.
 
-  This sensor uses SPI bus to communicate, specials pins are required to interface
-  Board:                                    SCLK        MISO        SS don't use for CS   MOSI
-  Uno, Mini, Pro, ATmega168, ATmega328..... 13          12          10                    11
-  Mega2560, Due............................ 52          50          53                    51
-  Leonardo, ProMicro, ATmega32U4........... 15          14          x                     16
-  Blue Pill, STM32F103xxxx boards.......... PA5**       PA6**       PA4**                 PA7**
-  NodeMCU 1.0, WeMos D1 Mini............... GPIO14/D5   GPIO12/D6   GPIO15/D8*            GPIO13/D7
+   This sensor uses SPI bus to communicate, specials pins are required to interface
+   Board:                                    MOSI        MISO        SCLK         SS, don't use for CS   Level
+   Uno, Mini, Pro, ATmega168, ATmega328..... 11          12          13           10                     5v
+   Mega, Mega2560, ATmega1280, ATmega2560... 51          50          52           53                     5v
+   Due, SAM3X8E............................. ICSP4       ICSP1       ICSP3        x                      3.3v
+   Leonardo, ProMicro, ATmega32U4........... 16          14          15           x                      5v
+   Blue Pill, STM32F103xxxx boards.......... PA17        PA6         PA5          PA4                    3v
+   NodeMCU 1.0, WeMos D1 Mini............... GPIO13/D7   GPIO12/D6   GPIO14/D5    GPIO15/D8*             3v/5v
+   ESP32.................................... GPIO23/D23  GPIO19/D19  GPIO18/D18   x                      3v
 
-                                           *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
-                                            pullup-down resistor
-                                          **STM32F103xxxx pins are NOT 5v tolerant, bi-directional
-                                            logic level converter is needed
+                                            *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
+                                             pullup-down resistor
+   Frameworks & Libraries:
+   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+   ESP32 Core            - https://github.com/espressif/arduino-esp32
+   ESP8266 Core          - https://github.com/esp8266/Arduino
+   ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
-  Frameworks & Libraries:
-  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
-  ESP8266 Core          - https://github.com/esp8266/Arduino
-  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
-  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
-
-  GNU GPL license, all text above must be included in any redistribution, see link below for details:
-  - https://www.gnu.org/licenses/licenses.html
+   GNU GPL license, all text above must be included in any redistribution, see link below for details:
+   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 #include <MAX31855.h>
@@ -45,12 +45,11 @@ int32_t rawData_01  = 0;
 int32_t rawData_02  = 0;
 
 /*
-- for harware spi
-
   MAX31855(cs)
 
-  cs  - chip select, if CS low serial interface is enabled
+  cs - chip select
 */
+
 MAX31855 myMAX31855_01(3); //for ESP8266 use D3
 MAX31855 myMAX31855_02(4); //for ESP8266 use D4
 
