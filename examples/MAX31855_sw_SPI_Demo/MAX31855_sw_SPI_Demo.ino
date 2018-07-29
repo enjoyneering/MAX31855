98e7ddbb2823f82e72f112474bb0f97e4d5803bf
enjoyneering <enjoyneering79@gmail.com>
2018-07-29 11:39:43 -0400

    Update MAX31855_sw_SPI_Demo.ino

--- examples/MAX31855_sw_SPI_Demo/MAX31855_sw_SPI_Demo.ino
+++ examples/MAX31855_sw_SPI_Demo/MAX31855_sw_SPI_Demo.ino
@@ -1,48 +1,45 @@
 /***************************************************************************************************/
 /* 
-  Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
+   Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
 
-  written by : enjoyneering79
-  sourse code: https://github.com/enjoyneering/MAX6675
+   written by : enjoyneering79
+   sourse code: https://github.com/enjoyneering/MAX6675
 
-  - MAX31855 maximum power supply voltage is 3.6v
-  - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
-  - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
-    with 0.25°C resolution/increment.
-  - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
-    Optimal performance of cold junction compensation happends when the thermocouple cold junction
-    & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
-    near the converter because this may produce an errors.
-  - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
-    the T+ and T- pins, to filter noise on the thermocouple lines.
+   - MAX31855 maximum power supply voltage is 3.6v
+   - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
+   - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
+     with 0.25°C resolution/increment.
+   - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
+     Optimal performance of cold junction compensation happends when the thermocouple cold junction
+     & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
+     near the converter because this may produce an errors.
+   - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
+     the T+ and T- pins, to filter noise on the thermocouple lines.
 
-  Frameworks & Libraries:
-  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
-  ESP8266 Core          - https://github.com/esp8266/Arduino
-  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
-  STM32 Core*           - https://github.com/rogerclarkmelbourne/Arduino_STM32
+   Frameworks & Libraries:
+   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+   ESP32 Core            - https://github.com/espressif/arduino-esp32
+   ESP8266 Core          - https://github.com/esp8266/Arduino
+   ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
-                          *STM32F103xxxx pins are NOT 5v tolerant, bi-directional
-                           logic level converter is needed
-
-  GNU GPL license, all text above must be included in any redistribution, see link below for details:
-  - https://www.gnu.org/licenses/licenses.html
+   GNU GPL license, all text above must be included in any redistribution, see link below for details:
+   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
-#include <MAX31855.h>
+#include <MAX31855soft.h>
 
 int32_t rawData = 0;
 
 /*
-- for soft spi
+  MAX31855soft(cs, so, sck)
 
-  MAX31855(cs, so, sck)
-
-  cs  - chip select, if CS low serial interface is enabled
+  cs  - chip select
   so  - serial data output
   sck - serial clock input
 */
-MAX31855 myMAX31855(3, 4, 7); //for ESP8266 use D3, D4, D7
+
+MAX31855soft myMAX31855(3, 4, 7); //for ESP8266 use D3, D4, D7
 
 
 void setup()
