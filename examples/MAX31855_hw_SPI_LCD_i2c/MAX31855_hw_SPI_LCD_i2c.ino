a8fc04896b5728b6bebdbf68fa93a12a6c8fcccf
enjoyneering <enjoyneering79@gmail.com>
2018-07-29 11:39:05 -0400

    Update MAX31855_hw_SPI_LCD_i2c.ino

--- examples/MAX31855_hw_SPI_LCD_i2c/MAX31855_hw_SPI_LCD_i2c.ino
+++ examples/MAX31855_hw_SPI_LCD_i2c/MAX31855_hw_SPI_LCD_i2c.ino
@@ -1,77 +1,77 @@
 /***************************************************************************************************/
 /* 
-  Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
+   Example for 12-bit MAX6675 K-Thermocouple to Digital Converter with Cold Junction Compensation
 
-  written by : enjoyneering79
-  sourse code: https://github.com/enjoyneering/MAX6675
+   written by : enjoyneering79
+   sourse code: https://github.com/enjoyneering/MAX6675
 
-  - MAX31855 maximum power supply voltage is 3.6v
-  - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
-  - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
-    with 0.25°C resolution/increment.
-  - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
-    Optimal performance of cold junction compensation happends when the thermocouple cold junction
-    & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
-    near the converter because this may produce an errors.
-  - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
-    the T+ and T- pins, to filter noise on the thermocouple lines.
+   - MAX31855 maximum power supply voltage is 3.6v
+   - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
+   - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
+     with 0.25°C resolution/increment.
+   - Cold junction compensation range -40°C..+125° ±3°C with 0.062°C resolution/increment.
+     Optimal performance of cold junction compensation happends when the thermocouple cold junction
+     & the MAX31855 are at the same temperature. Avoid placing heat-generating devices or components
+     near the converter because this may produce an errors.
+   - It is strongly recommended to add a 10nF/0.01mF ceramic surface-mount capacitor, placed across
+     the T+ and T- pins, to filter noise on the thermocouple lines.
 
-  This lcd uses I2C bus to communicate, specials pins are required to interface
-  Board:                                    SDA                    SCL
-  Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5
-  Mega2560, Due............................ 20                     21
-  Leonardo, Micro, ATmega32U4.............. 2                      3
-  Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7
-  Blue Pill, STM32F103xxxx boards.......... PB7*                   PB6*
-  ESP8266 ESP-01:.......................... GPIO0/D5               GPIO2/D3
-  NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1
+   This lcd uses I2C bus to communicate, specials pins are required to interface
+   Board:                                    SDA                    SCL                    Level
+   Uno, Mini, Pro, ATmega168, ATmega328..... A4                     A5                     5v
+   Mega2560................................. 20                     21                     5v
+   Due, SAM3X8E............................. 20                     21                     3.3v
+   Leonardo, Micro, ATmega32U4.............. 2                      3                      5v
+   Digistump, Trinket, ATtiny85............. 0/physical pin no.5    2/physical pin no.7    5v
+   Blue Pill, STM32F103xxxx boards.......... PB7                    PB6                    3.3v/5v
+   ESP8266 ESP-01........................... GPIO0/D5               GPIO2/D3               3.3v/5v
+   NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1               3.3v/5v
+   ESP32.................................... GPIO21/D21             GPIO22/D22             3.3v
 
-                                           *STM32F103xxxx pins B7/B7 are 5v tolerant, but bi-directional
-                                            logic level converter is recommended
+   This sensor uses SPI bus to communicate, specials pins are required to interface
+   Board:                                    MOSI        MISO        SCLK         SS, don't use for CS   Level
+   Uno, Mini, Pro, ATmega168, ATmega328..... 11          12          13           10                     5v
+   Mega, Mega2560, ATmega1280, ATmega2560... 51          50          52           53                     5v
+   Due, SAM3X8E............................. ICSP4       ICSP1       ICSP3        x                      3.3v
+   Leonardo, ProMicro, ATmega32U4........... 16          14          15           x                      5v
+   Blue Pill, STM32F103xxxx boards.......... PA17        PA6         PA5          PA4                    3v
+   NodeMCU 1.0, WeMos D1 Mini............... GPIO13/D7   GPIO12/D6   GPIO14/D5    GPIO15/D8*             3v/5v
+   ESP32.................................... GPIO23/D23  GPIO19/D19  GPIO18/D18   x                      3v
 
-  This sensor uses SPI bus to communicate, specials pins are required to interface
-  Board:                                    SCLK        MISO        SS don't use for CS   MOSI
-  Uno, Mini, Pro, ATmega168, ATmega328..... 13          12          10                    11
-  Mega2560, Due............................ 52          50          53                    51
-  Leonardo, ProMicro, ATmega32U4........... 15          14          x                     16
-  Blue Pill, STM32F103xxxx boards.......... PA5**       PA6**       PA4**                 PA7**
-  NodeMCU 1.0, WeMos D1 Mini............... GPIO14/D5   GPIO12/D6   GPIO15/D8*            GPIO13/D7
+                                            *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
+                                             pullup-down resistor
+   Frameworks & Libraries:
+   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
+   ESP32 Core            - https://github.com/espressif/arduino-esp32
+   ESP8266 Core          - https://github.com/esp8266/Arduino
+   ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
+   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
-                                           *if GPIO2/D4 or GPIO0/D3 used for for CS, apply an external 25kOhm
-                                            pullup-down resistor
-                                          **STM32F103xxxx pins are NOT 5v tolerant, bi-directional
-                                            logic level converter is needed
-
-  Frameworks & Libraries:
-  ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
-  ESP8266 Core          - https://github.com/esp8266/Arduino
-  ESP8266 I2C lib fixed - https://github.com/enjoyneering/ESP8266-I2C-Driver
-  STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
-
-  GNU GPL license, all text above must be included in any redistribution, see link below for details:
-  - https://www.gnu.org/licenses/licenses.html
+   GNU GPL license, all text above must be included in any redistribution, see link below for details:
+   - https://www.gnu.org/licenses/licenses.html
 */
 /***************************************************************************************************/
 #include <SPI.h>
-#include <Wire.h>
+#include <Wire.h>               //for esp8266 use bug free i2c driver https://github.com/enjoyneering/ESP8266-I2C-Driver
 #include <MAX31855.h>
 #include <LiquidCrystal_I2C.h>  //https://github.com/enjoyneering/LiquidCrystal_I2C
 
 #define LCD_ROWS           4    //qnt. of lcd rows
 #define LCD_COLUMNS        20   //qnt. of lcd columns
-#define LCD_DEGREE_SYMBOL  0xDF //degree symbol from lcd ROM
-#define LCD_SPACE_SYMBOL   0x20 //space  symbol from lcd ROM
 
-#define MAX_TEMPERATURE    45   //max temp, deg.C
+#define LCD_DEGREE_SYMBOL  0xDF //degree symbol from lcd ROM, see p.9 of GDM2004D datasheet
+#define LCD_SPACE_SYMBOL   0x20 //space  symbol from lcd ROM, see p.9 of GDM2004D datasheet
 
-const uint8_t icon_temperature[8] PROGMEM = {0x04, 0x0E, 0x0E, 0x0E, 0x0E, 0x1F, 0x1F, 0x0E}; //PROGMEM saves variable to flash & keeps dynamic memory free
+#define MAX_TEMPERATURE    45   //max temp, in °C
+
+const uint8_t iconTemperature[8] PROGMEM = {0x04, 0x0E, 0x0E, 0x0E, 0x0E, 0x1F, 0x1F, 0x0E}; //PROGMEM saves variable to flash & keeps dynamic memory free
 
 float temperature = 0;
 
 /*
 MAX31855(cs)
 
-cs  - chip select, if CS low serial interface is enabled
+cs - chip select
 */
 
 MAX31855          myMAX31855(4); //chip select pin 
@@ -85,10 +85,10 @@ void setup()
   /* LCD connection check */  
   while (lcd.begin(LCD_COLUMNS, LCD_ROWS, LCD_5x8DOTS) != true) //20x4 display, 5x8 pixels size
   {
-    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal.")); //(F()) saves string to flash & keeps dynamic memory free
+    Serial.println(F("PCF8574 is not connected or lcd pins declaration is wrong. Only pins numbers: 4,5,6,16,11,12,13,14 are legal."));
     delay(5000);
   }
-  lcd.print(F("PCF8574 is OK"));
+  lcd.print(F("PCF8574 is OK")); //(F()) saves string to flash & keeps dynamic memory free
   delay(1000);
 
   lcd.clear();
@@ -113,11 +113,17 @@ void setup()
       case MAX31855_THERMOCOUPLE_SHORT_TO_VCC:
         lcd.print(F("short to Vcc "));
         break;
+
       case MAX31855_THERMOCOUPLE_SHORT_TO_GND:
         lcd.print(F("short to GND "));
         break;
+
       case MAX31855_THERMOCOUPLE_NOT_CONNECTED:
         lcd.print(F("not connected"));
+        break;
+
+      case MAX31855_THERMOCOUPLE_UNKNOWN:
+        lcd.print(F("unknown error"));                            //check spi cable
         break;
     }
     delay(5000);
@@ -131,7 +137,7 @@ void setup()
   lcd.clear();
 
   /* load custom symbol to CGRAM */
-  lcd.createChar(0, icon_temperature);
+  lcd.createChar(0, iconTemperature);
 
   /* prints static text */
   lcd.setCursor(0, 0);
