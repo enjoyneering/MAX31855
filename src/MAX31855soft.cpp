113db224b5cde499b38fee1000aea7c0487e5099
enjoyneering <enjoyneering79@gmail.com>
2019-11-25 08:23:33 -0500

    Update MAX31855soft.cpp

--- src/MAX31855soft.cpp
+++ src/MAX31855soft.cpp
@@ -124,8 +124,8 @@ int32_t MAX31855soft::readRawData(void)
   digitalWrite(_cs, HIGH);                       //start measurement/conversion
   delay(MAX31855_CONVERSION_TIME);
 
-  digitalWrite(_cs, LOW);                        //set CS low to enable SPI interface for MAX31855
   digitalWrite(_sck, LOW);
+  digitalWrite(_cs, LOW);                        //set CS low to enable SPI interface for MAX31855
 
   for (int8_t i = 32; i > 0; i--)                //read 32-bits via software SPI, in order MSB->LSB (D31..D0 bit)
   {
