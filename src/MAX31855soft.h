fc2fa331ce0ba84a4c7971411911542f7c6079df
enjoyneering <enjoyneering79@gmail.com>
2020-01-28 13:13:01 -0500

    Update MAX31855soft.h

--- src/MAX31855soft.h
+++ src/MAX31855soft.h
@@ -27,15 +27,14 @@
    NodeMCU 1.0, WeMos D1 Mini...............  3v/5v*
    ESP32....................................  3v
 
-                                             *if GPIO2/D4 or GPIO0/D3 is used, apply an external
-                                              25kOhm pullup-down resistor otherwise reset & reset button
-                                              may not work
+                                              *most boards has 10-12kOhm pullup-up resistor on GPIO2/D4 & GPIO0/D3
 
    Frameworks & Libraries:
-   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
-   ESP32 Core            - https://github.com/espressif/arduino-esp32
+   ATtiny  Core          - https://github.com/SpenceKonde/ATTinyCore
+   ESP32   Core          - https://github.com/espressif/arduino-esp32
    ESP8266 Core          - https://github.com/esp8266/Arduino
-   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+   STM32   Core          - https://github.com/stm32duino/Arduino_Core_STM32
+                         - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
    GNU GPL license, all text above must be included in any redistribution,
    see link for details  - https://www.gnu.org/licenses/licenses.html
