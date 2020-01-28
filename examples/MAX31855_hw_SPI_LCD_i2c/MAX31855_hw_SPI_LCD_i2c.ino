d223ad97e45cca6afa44fa853237eee318c4c03c
enjoyneering <enjoyneering79@gmail.com>
2020-01-28 18:25:25 -0500

    Update MAX31855_hw_SPI_LCD_i2c.ino

--- examples/MAX31855_hw_SPI_LCD_i2c/MAX31855_hw_SPI_LCD_i2c.ino
+++ examples/MAX31855_hw_SPI_LCD_i2c/MAX31855_hw_SPI_LCD_i2c.ino
@@ -27,6 +27,7 @@
    ESP8266 ESP-01........................... GPIO0/D5               GPIO2/D3               3.3v/5v
    NodeMCU 1.0, WeMos D1 Mini............... GPIO4/D2               GPIO5/D1               3.3v/5v
    ESP32.................................... GPIO21/D21             GPIO22/D22             3.3v
+
    This sensor uses SPI bus to communicate, specials pins are required to interface
    Board:                                    MOSI        MISO        SCLK         SS, don't use for CS   Level
    Uno, Mini, Pro, ATmega168, ATmega328..... 11          12          13           10                     5v
@@ -37,15 +38,15 @@
    NodeMCU 1.0, WeMos D1 Mini............... GPIO13/D7   GPIO12/D6   GPIO14/D5    GPIO15/D8*             3v/5v
    ESP32.................................... GPIO23/D23  GPIO19/D19  GPIO18/D18   x                      3v
 
-                                             *if GPIO2/D4 or GPIO0/D3 is used for for CS, apply an external
-                                              25kOhm pullup-down resistor otherwise reset & reset button
-                                              may not work
+                                             *most boards has 10-12kOhm pullup-up resistor on GPIO2/D4 & GPIO0/D3
+                                              for flash & boot
 
    Frameworks & Libraries:
-   ATtiny Core           - https://github.com/SpenceKonde/ATTinyCore
-   ESP32 Core            - https://github.com/espressif/arduino-esp32
+   ATtiny  Core          - https://github.com/SpenceKonde/ATTinyCore
+   ESP32   Core          - https://github.com/espressif/arduino-esp32
    ESP8266 Core          - https://github.com/esp8266/Arduino
-   STM32 Core            - https://github.com/rogerclarkmelbourne/Arduino_STM32
+   STM32   Core          - https://github.com/stm32duino/Arduino_Core_STM32
+                         - https://github.com/rogerclarkmelbourne/Arduino_STM32
 
    GNU GPL license, all text above must be included in any redistribution,
    see link for details  - https://www.gnu.org/licenses/licenses.html
@@ -74,7 +75,7 @@ MAX31855(cs)
 cs - chip select
 */
 
-MAX31855          myMAX31855(4); //chip select pin 
+MAX31855          myMAX31855(4); //chip select pin, for ESP8266 change to D4 (fails to BOOT/FLASH if pin LOW)
 LiquidCrystal_I2C lcd(PCF8574_ADDR_A21_A11_A01, 4, 5, 6, 16, 11, 12, 13, 14, POSITIVE);
 
 
