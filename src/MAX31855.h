8917dec796535f5290974f1bbdbf41f624ff7be3
enjoyneering <enjoyneering79@gmail.com>
2020-06-22 09:18:25 -0400

    Update MAX31855.h

--- src/MAX31855.h
+++ src/MAX31855.h
@@ -5,6 +5,7 @@
    rate ~9..10Hz.
 
    - MAX31855 maximum power supply voltage is 3.6v
+   - Maximum SPI bus speed 5Mhz
    - K-type thermocouples have an absolute accuracy of around ±2°C..±6°C.
    - Measurement tempereture range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C
      with 0.25°C resolution/increment.
@@ -43,61 +44,214 @@
 */
 /***************************************************************************************************/
 
-#ifndef MAX31855_h
-#define MAX31855_h
-
-#if defined(ARDUINO) && ((ARDUINO) >= 100) //arduino core v1.0 or later
-#include <Arduino.h>
-#else
-#include <WProgram.h>
-#endif
-
-#if defined(__AVR__)
-#include <avr/pgmspace.h>                  //use for PROGMEM Arduino AVR
-#elif defined(ESP8266)
-#include <pgmspace.h>                      //use for PROGMEM Arduino ESP8266
-#elif defined(_VARIANT_ARDUINO_STM32_)
-#include <avr/pgmspace.h>                  //use for PROGMEM Arduino STM32
-#endif
-
-#ifndef  MAX31855_SOFT_SPI                 //enable upload hw driver spi.h
-#include <SPI.h>
-#endif
+#include <MAX31855.h>
 
 
-#define MAX31855_CONVERSION_POWER_UP_TIME   200    //in milliseconds
-#define MAX31855_CONVERSION_TIME            100    //in milliseconds, 9..10Hz sampling rate 
-#define MAX31855_THERMOCOUPLE_RESOLUTION    0.25   //in °C per dac step
-#define MAX31855_COLD_JUNCTION_RESOLUTION   0.0625 //in °C per dac step
+/**************************************************************************/
+/*
+    MAX31855()
 
+    Constructor for hardware read only SPI
 
-#define MAX31855_ID                         31855
-#define MAX31855_FORCE_READ_DATA            7      //force to read the data, 7 is unique because d2d1d0 can't be all high at the same time
-#define MAX31855_ERROR                      2000   //returned value if any error happends
-
-#define MAX31855_THERMOCOUPLE_OK            0
-#define MAX31855_THERMOCOUPLE_SHORT_TO_VCC  1
-#define MAX31855_THERMOCOUPLE_SHORT_TO_GND  2
-#define MAX31855_THERMOCOUPLE_NOT_CONNECTED 3
-#define MAX31855_THERMOCOUPLE_UNKNOWN       4
-
-
-class MAX31855
+    NOTE:
+    - cs is chip select, set cs low to enable serial interface
+*/
+/**************************************************************************/
+MAX31855::MAX31855(uint8_t cs)
 {
-  public:
-   MAX31855(uint8_t cs);
+  _cs = cs; //cs chip select
+}
 
-           void     begin(void);
-           uint8_t  detectThermocouple(int32_t rawValue = MAX31855_FORCE_READ_DATA);
-           uint16_t getChipID(int32_t rawValue = MAX31855_FORCE_READ_DATA);
-           float    getTemperature(int32_t rawValue = MAX31855_FORCE_READ_DATA);
-           float    getColdJunctionTemperature(int32_t rawValue = MAX31855_FORCE_READ_DATA);
-   virtual int32_t  readRawData(void);
- 
-  private:
+/**************************************************************************/
+/*
+    begin()
 
-  protected:
-   uint8_t _cs;
-};
+    Initializes & configures hardware SPI
+*/
+/**************************************************************************/
+void MAX31855::begin(void)
+{
+  pinMode(_cs, OUTPUT);
+  digitalWrite(_cs, HIGH);                  //disables SPI interface for MAX31855, but it will initiate measurement/conversion
 
-#endif
+  SPI.begin();                              //setting hardware SCK, MOSI, SS to output, pull SCK, MOSI low & SS high    
+
+  delay(MAX31855_CONVERSION_POWER_UP_TIME);
+}
+
+/**************************************************************************/
+/*
+    detectThermocouple()
+
+    Checks if thermocouple is open, shorted to GND, shorted to VCC
+
+    Return:
+    - 0 OK
+    - 1 short to VCC
+    - 2 short to GND
+    - 3 not connected
+
+    NOTE:
+    - bit D16 is normally low & goes high if thermocouple is open, shorted to GND or VCC
+    - bit D2  is normally low & goes high to indicate a hermocouple short to VCC
+    - bit D1  is normally low & goes high to indicate a thermocouple short to GND
+    - bit D0  is normally low & goes high to indicate a thermocouple open circuit
+*/
+/**************************************************************************/
+uint8_t MAX31855::detectThermocouple(int32_t rawValue)
+{
+  if (rawValue == MAX31855_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (rawValue == 0)                    return MAX31855_THERMOCOUPLE_READ_FAIL;
+
+  if (bitRead(rawValue, 16) == 1)
+  {
+    if      (bitRead(rawValue, 2) == 1) return MAX31855_THERMOCOUPLE_SHORT_TO_VCC;
+    else if (bitRead(rawValue, 1) == 1) return MAX31855_THERMOCOUPLE_SHORT_TO_GND;
+    else if (bitRead(rawValue, 0) == 1) return MAX31855_THERMOCOUPLE_NOT_CONNECTED;
+    else                                return MAX31855_THERMOCOUPLE_UNKNOWN;
+  }
+  return MAX31855_THERMOCOUPLE_OK;
+}
+
+/**************************************************************************/
+/*
+    getChipID()
+
+    Checks chip ID
+
+    NOTE:
+    - bit D17, D3 always return zero & can be used as device ID
+*/
+/**************************************************************************/
+uint16_t MAX31855::getChipID(int32_t rawValue)
+{
+  if (rawValue == MAX31855_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (rawValue == 0)                                           return MAX31855_THERMOCOUPLE_READ_FAIL;
+  if (bitRead(rawValue, 17) == 0 && bitRead(rawValue, 3) == 0) return MAX31855_ID;
+
+  return MAX31855_ERROR;
+}
+
+/**************************************************************************/
+/*
+    getTemperature()
+
+    Reads Temperature, C
+
+    NOTE:
+    - range -200°C..+700°C ±2°C or -270°C..+1372°C ±6°C with 0.25°C
+      resolution/increment
+    - thermocouple temperature data is 14-bit long
+    - bit D31 is the thermocouple temperature sign bit "+" is high & "-" is low,
+      if T+ and T- are unconnected it goes low
+    - bits D30..D18 contain the converted temperature in the order of MSB to LSB,
+      if T+ and T- are unconnected they go high
+    - it is strongly recommended to add a 10nF/0.01mF ceramic surface-mount
+      capacitor, placed across the T+ and T- pins, to filter noise on the
+      thermocouple lines
+*/
+/**************************************************************************/
+float MAX31855::getTemperature(int32_t rawValue)
+{
+  if (rawValue == MAX31855_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (detectThermocouple(rawValue) != MAX31855_THERMOCOUPLE_OK) return MAX31855_ERROR;
+
+  rawValue = rawValue >> 18; //clear D17..D0 bits
+
+  return (float)rawValue * MAX31855_THERMOCOUPLE_RESOLUTION;
+}
+
+/**************************************************************************/
+/*
+    getColdJunctionTemperature()
+
+    Reads Temperature, C
+
+    NOTE:
+    - range -40°C..+125° ±3°C with 0.062°C resolution/increment
+    - chip internal temperature data is 12-bit long
+    - bit D15 is cold-junction temperature sign bit "+" is high & "-" is low
+    - bits D14..D4 contain cold-junction temperature in the order of MSB to LSB
+*/
+/**************************************************************************/
+float MAX31855::getColdJunctionTemperature(int32_t rawValue)
+{
+  if (rawValue == MAX31855_FORCE_READ_DATA) rawValue = readRawData();
+
+  if (getChipID(rawValue) != MAX31855_ID) return MAX31855_ERROR;
+
+  rawValue = rawValue & 0x0000FFFF; //clear D31..D16 bits
+  rawValue = rawValue >> 4;         //clear D3...D0  bits
+
+  return (float)rawValue * MAX31855_COLD_JUNCTION_RESOLUTION;
+}
+
+/**************************************************************************/
+/*
+    readRawData()
+
+    Reads raw data from MAX31855 via hardware SPI
+
+    NOTE:
+    - max SPI clock speed for MAX31855 is 5MHz
+    - in SPI_MODE0 data available shortly after the rising edge of SCK
+    - read of the cold-junction compensated thermocouple temperature requires
+      14 clock cycles
+    - read of the cold-junction compensated thermocouple temperature & reference
+      junction temperatures requires 32 clock cycles
+    - forcing CS low immediately stops any conversion process, force CS high
+      to initiate a new measurement process
+    - set CS low to enable the serial interface & force to output the first bit on the SO pin,
+      apply 14/32 clock signals at SCK to read the results at SO on the falling edge of the SCK
+    - bit D31 is the thermocouple temperature sign bit "+" is high & "-" is low,
+      if T+ & T- pins are unconnected it goes low
+    - bits D30..D18 contain the converted temperature in the order of MSB to LSB,
+      if T+ & T- pins are unconnected they go high
+    - bit D17 is low to provide a device ID for the MAX31855
+    - bit D16 is normally low & goes high if thermocouple is open, shorted to GND or VCC
+    - bit D15 is cold-junction temperature sign bit "+" is high & "-" is low
+    - bits D14..D4 contain cold-junction temperature in the order of MSB to LSB
+    - bit D3 is is low to provide a device ID for the MAX31855
+    - bit D2 is normally low & goes high to indicate a hermocouple short to VCC
+    - bit D1 is normally low & goes high to indicate a thermocouple short to GND
+    - bit D0 is normally low & goes high to indicate a thermocouple open circuit
+
+    - 8-bit  16MHz  AVR  one clock cycle is 62.5nS
+    - 32-bit 80MHz/180 ESP8266 one clock cycle is 12.5nS/5.5nS
+    - 32-bit 160MHz/250MHz ESP32 one clock cycle is 6.25nS/4nS
+    - 32-bit 72MHz STM32 one clock cycle is 13.9nS
+    - arduino 8-bit AVR maximum SPI master clock speed is mcu speed/2,
+      for 5v-16MHz/ProMini speed is 16000000/2=8MHz 
+    - arduino ESP8266 maximum SPI master clock speed is 80000000=80MHz
+    - arduino STM32 maximum SPI master clock speed is mcu speed/2,
+      for STM32F103C8 speed is 72000000/2=36MHz
+*/
+/**************************************************************************/
+int32_t MAX31855::readRawData(void)
+{
+  int32_t rawData = 0;
+
+  digitalWrite(_cs, LOW);                                          //stop  measurement/conversion
+  delayMicroseconds(1);                                            //pulse fall time > 100nS
+  digitalWrite(_cs, HIGH);                                         //start measurement/conversion
+
+  delay(MAX31855_CONVERSION_TIME);
+
+  SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0)); //up to 5MHz, read MSB first, SPI mode 0, see note
+
+  digitalWrite(_cs, LOW);                                          //set software CS low to enable SPI interface for MAX31855
+
+  for (uint8_t i = 0; i < 2; i++)                                  //read 32-bits via hardware SPI, in order MSB->LSB (D31..D0 bit)
+  {
+    rawData = (rawData << 16) | SPI.transfer16(0x0000);            //chip has read only SPI & MOSI not connected, so it doesn't metter what to send
+  }
+
+  digitalWrite(_cs, HIGH);                                         //disables SPI interface for MAX31855, but it will initiate measurement/conversion
+
+  SPI.endTransaction();                                            //de-asserting hardware CS & free hw SPI for other slaves
+
+  return rawData;
+}
